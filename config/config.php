a194f4db1988e05d67e7754c5e9ee332170b2999
Janos Pasztor <janos@pasztor.at>
2016-07-12 08:36:38 +0200

    Added better config documentation

--- config/config.php
+++ config/config.php
@@ -5,26 +5,43 @@ use AcmeCorp\App\Web\ErrorController;
 
 use Piccolo\Web\HTTP\Guzzle\GuzzleHTTPModule;
 use Piccolo\Templating\Engine\Twig\TwigTemplatingModule;
-use Piccolo\Templating\TemplatingModule;
 use Piccolo\Web\IO\Standard\StandardWebIOModule;
 use Piccolo\Web\Processor\Controller\ControllerProcessorWebModule;
-use Piccolo\Web\Processor\Controller\View\Templating\TemplateViewProcessor;
 use Piccolo\Web\Processor\Controller\View\Templating\TemplatingViewModule;
 use Piccolo\Web\Routing\FastRoute\FastRouteModule;
-use Piccolo\Web\WebModule;
 
 return [
 	/**
 	 * Piccolo modules to load. These modules will consume the configuration below.
 	 */
 	'modules' => [
-		WebModule::class,
+		/**
+		 * Use Guzzle for HTTP request and response constructions
+		 */
 		GuzzleHTTPModule::class,
+		/**
+		 * Routing and controller handling
+		 */
 		ControllerProcessorWebModule::class,
+		/**
+		 * Standard PHP input and output mechanisms
+		 */
 		StandardWebIOModule::class,
+		/**
+		 * Use FastRoute for routing
+		 */
 		FastRouteModule::class,
+		/**
+		 * Use templating for the view
+		 */
 		TemplatingViewModule::class,
+		/**
+		 * Use Twig as the templating engine
+		 */
 		TwigTemplatingModule::class,
+		/**
+		 * Our application here
+		 */
 		ApplicationModule::class,
 	],
 	/**
@@ -33,11 +50,19 @@ return [
 	 * @see https://github.com/nikic/FastRoute
 	 */
 	'fastroute' => [
+		/**
+		 * Error handlers. The fastroute module supports 404, 405 and 500 error handlers.
+		 */
 		'errorHandlers' => [
 			404 => [ErrorController::class, 'notFound'],
 			405 => [ErrorController::class, 'methodNotAllowed'],
 			500 => [ErrorController::class, 'error'],
 		],
+		/**
+		 * Routes. Examples:
+		 * 
+		 * ['GET',  '/{slug:[a-zA-Z\-]+}', BlogController::class, 'postAction'],
+		 */
 		'routes' => [
 		],
 	],
